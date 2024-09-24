f7e973926d1e5149ed7e102c30075497e8f4b076
Ken (V.H) Nguyen <ken.nguyen@ing.com>
2024-09-24 09:43:46 +0200

    Test failed using configuration property due to double quotes

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -15,6 +15,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST";
+        return greeting;
     }
 }
